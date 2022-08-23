281c244c2cd5476cb16fbc38f58aafee4f3f1b2a
Olivier Biesmans <olivier.biesmans@prevision.io>
2022-08-23 12:11:22 +0200

    Add pattern for indent highlight of yaml files

--- nvim/lua/user/identblankline.lua
+++ nvim/lua/user/identblankline.lua
@@ -2,4 +2,24 @@ require("indent_blankline").setup({
 	-- show_end_of_line = true
 	indent_blankline_use_treesitter = true,
 	show_current_context = true,
+	context_patterns = {
+		"^block_", -- highlight context patterns for yaml
+		"class",
+		"^func",
+		"method",
+		"^if",
+		"while",
+		"for",
+		"with",
+		"try",
+		"except",
+		"arguments",
+		"argument_list",
+		"object",
+		"dictionary",
+		"element",
+		"table",
+		"tuple",
+		"do_block",
+	},
 })
