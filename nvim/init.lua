5f58caf2d4743a8967af7daf95a2ce3a174c84a5
Olivier Biesmans <olivier.biesmans@prevision.io>
2022-11-14 10:53:47 +0100

    add neovide

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
 require('packages')
 
+--
+-- Options
+--
 
 vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.completeopt = "menu,menuone,noselect"
@@ -7,6 +10,16 @@ vim.g.mapleader = ","
 
 vim.opt.cursorlineopt = "number"
 vim.opt.cursorline = true
+
+--
+vim.opt.mouse = "a" -- enables mouse support for all modes
+vim.api.nvim_command("behave xterm") -- Xterm behavior for mouse and selection
+
+-- GUI Options
+if vim.fn.has("gui_running") then
+	vim.opt.guifont = { "FiraCode_Nerd_Font_Mono", ":h12" }
+end
+
 
 
 require('plugins')
@@ -20,7 +33,11 @@ end
 
 -- local border_opts = { border = 'rounded', focusable = false, scope = 'line' }
 
-vim.diagnostic.config()
+-- neovide
+if vim.fn.exists("g:neovide") then
+	vim.g.neovide_cursor_animation_length = 0 -- disable cursor animation
+	vim.api.nvim_set_keymap('i', '<S-Insert>', '<C-R>*', { noremap = true, silent = true }) -- shift-insert inserts selection clipboard
+end
 
 --vim.api.nvim_create_autocmd(
 --  {"CursorHold"},
@@ -29,6 +46,3 @@ vim.diagnostic.config()
 --      vim.lsp.diagnostic.show()
 --    end}
 --)
---
---
---
