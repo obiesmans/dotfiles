c0073f89d167ac18eae87d79721f7a966551bdee
Olivier Biesmans <olivier.biesmans@prevision.io>
2022-11-29 14:04:34 +0100

    change statusline to global

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,18 @@
+local function statusline()
+	local position = vim.fn['nvim_treesitter#statusline']()
+	if position then
+		return position
+	end
+end
+
 require('lualine').setup {
-  sections = {
-    lualine_c = {
-      'filename',
-      function() -- display code context in statusline
-        return vim.fn['nvim_treesitter#statusline']()
-      end
-    }
-  }
+	options = {
+		globalstatus = true, -- enable single statusline at bottom of neovim instead of one for  every window.
+	},
+	sections = {
+		lualine_c = {
+			'filename',
+			statusline
+		}
+	}
 }
